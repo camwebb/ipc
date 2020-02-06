1aee1d821eebd8440ef919f0787c96318b0d3f5f
Cam Webb <cw@camwebb.info>
2020-02-05 20:32:05 -0900

    auto

--- db/tables/person/person.php
+++ db/tables/person/person.php
@@ -3,10 +3,12 @@
 class tables_person {
     
     // only allow access to this table if logged in
+    /* 
     function getPermissions(&$record){
         $auth =& Dataface_AuthenticationTool::getInstance();
         $user =& $auth->getLoggedInUser();
         if ( !isset($user) ) return Dataface_PermissionsTool::NO_ACCESS();
     }
+    */
 }
 ?>
